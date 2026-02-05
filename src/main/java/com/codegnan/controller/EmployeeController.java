7b3524751f70bd117f5112c0a8fe568f22035481
Sathwik3010 <psdev2002@gmail.com>
2026-02-05 12:10:15 +0530

    spring boot mvc

---
+++ src/main/java/com/codegnan/controller/EmployeeController.java
@@ -0,0 +1,65 @@
+package com.codegnan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.codegnan.entity.Employee;
+import com.codegnan.service.EmployeeService;
+
+@Controller
+public class EmployeeController {
+	private EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService employeeService) {
+		super();
+		this.employeeService = employeeService;
+	}
+	
+	@RequestMapping("/")
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView("employee-list");
+		mav.addObject("employees", employeeService.getAllEmployees());
+		return mav;
+	}
+	
+	@RequestMapping("/addEmployeeForm")
+	public ModelAndView addEmployeeForm() {
+		return new ModelAndView("add-employee-form");
+	}
+	
+	@RequestMapping("/addEmployee")
+	public ModelAndView addEmployee(@RequestParam("name")String name,
+			@RequestParam("salary") Double salary,
+			@RequestParam("designation")String designation) {
+		Employee employee = new Employee(name, salary, designation);
+		employeeService.saveEmployee(employee);	
+		return new ModelAndView("redirect:/");
+		}
+	
+	@RequestMapping("/editEmployeeForm")
+	public ModelAndView editEmployeeForm(@RequestParam Long id) {
+		ModelAndView mav = new ModelAndView("edit-employee-form");
+		Employee employee = employeeService.getEmployeeById(id);
+		mav.addObject("employee", employee);
+		return mav;
+	}
+	
+	@RequestMapping("/editEmployee")
+	public ModelAndView editEmployee(@RequestParam Long id, @RequestParam String name, 
+			@RequestParam Double salary, @RequestParam String designation) {
+		Employee employee = employeeService.getEmployeeById(id);
+		employee.setName(name);
+		employee.setSalary(salary);
+		employee.setDesignation(designation);
+		employeeService.saveEmployee(employee);
+		return new ModelAndView("redirect:/");
+	}
+	
+	@RequestMapping("/deleteEmployee")
+	public ModelAndView deleteEmployee(@RequestParam Long id) {
+		employeeService.deleteEmployee(id);
+		return new ModelAndView("redirect:/");
+	}
+}
